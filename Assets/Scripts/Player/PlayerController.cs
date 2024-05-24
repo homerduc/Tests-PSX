89f9a670b23bdff09c54efb5b17b707a414d4737
homerduc <alexandre.duc00@orange.fr>
2024-05-24 23:30:25 +0200

    PlayerController fonctionnel et FollowPlayer supprimé
    je suis con en fait si on met l'objet camera en tant qu'objet enfant du player bah ça le suit tout seul

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public Camera playerCamera;
-    public float walkSpeed = 6.0f;
-    public float runSpeed = 12.0f;
+
+    [Header("Movement Settings")]
+    public float walkSpeed = 5.0f;
+    public float runSpeed = 7.0f;
     public float jumpPower = 7.0f;
-    public float gravity = 10.0f;
-
-
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
-
-
-    Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
-
+    public float gravity = 11.0f;
     public bool canMove = true;
 
+
+    [Header("Look Settings")]
+    public float lookSpeed = 2.0f;
+    public float lookYLimit = 45.0f;
+
+    [Header("References")]
+    public Camera playerCamera;
+
+    #region Initializations
     CharacterController controller;
+    Vector3 moveDirection = Vector3.zero;
+    float movementDirectionY;
+    float rotationX = 0;
+    float rotationY = 0;
+    #endregion
+
 
     void Start()
     {
@@ -32,7 +39,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        #region Mouvement
+        if (canMove)
+        {
+            HandleMovement();
+            HandleJump();
+            HandleRotation();
+        }
+
+        ApplyMovement();
+    }
+
+    /// <summary>
+    /// Contrôle toute la partie du mouvement sur le plan horizontal
+    /// </summary>
+    private void HandleMovement()
+    {
         // On prend les valeurs x et z de l'orientation actuelle locale du joueur pour les convertir en valeurs globales
         // Donc maintenant forward et right remplacent Vector3.forward et Vector3.right
         Vector3 forward = transform.TransformDirection(Vector3.forward);
@@ -49,9 +70,61 @@ public class PlayerController : MonoBehaviour
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
 
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        // On applique le mouvement du plan horizontal au vecteur moveDirection
+        moveDirection.x = (forward.x * curSpeedX) + (right.x * curSpeedY);
+        moveDirection.z = (forward.z * curSpeedX) + (right.z * curSpeedY);
+    }
 
+    /// <summary>
+    /// Contrôle toute la partie du mouvement sur le plan horizontal (saut et chute)
+    /// </summary>
+    private void HandleJump()
+    {
+        // Si les conditions sont réunies on saute
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+        // On applique la gravité
+        else if (!controller.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        // Sinon on maintient la vélocité précédente sur l'axe y (pour faire décoller ou bien retomber le joueur)
+        else
+        {
+            moveDirection.y = 0;
+        }
+    }
+
+    /// <summary>
+    /// Contrôle la rotation de la caméra et du joueur à l'aide de la souris
+    /// </summary>
+    private void HandleRotation()
+    {
+        if (canMove)
+        {
+            // En rotation, l'axe X représente les mouvements verticaux
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationY = Input.GetAxis("Mouse X") * lookSpeed;
+
+            // On limite les vues vers le bas et le haut
+            rotationX = Mathf.Clamp(rotationX, -lookYLimit, lookYLimit);
+
+            // On applique la rotation sur l'axe vertical
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+            // On applique la rotation sur l'axe vertical
+            transform.rotation *= Quaternion.Euler(0, rotationY, 0);
+        }
+    }
+
+    /// <summary>
+    /// Applique au Character Controller le vecteur de déplacement moveDirection qui contient toutes les forces à appliquer lors de cette frame
+    /// </summary>
+    private void ApplyMovement()
+    {
+        // On applique toutes les forces au character controller en fonction du temps écoulé par frame
         controller.Move(moveDirection * Time.deltaTime);
-        #endregion
     }
 }
